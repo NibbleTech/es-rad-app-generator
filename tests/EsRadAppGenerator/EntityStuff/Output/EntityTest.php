6ce462a5875f1c16ec3c3d31fa48b8792f135f7c
Shaun Walker <shaunandrewwalker@gmail.com>
2022-01-18 21:34:15 +0000

    Tests for Event and Entity classes

---
+++ tests/EsRadAppGenerator/EntityStuff/Output/EntityTest.php
@@ -0,0 +1,57 @@
+<?php
+declare(strict_types=1);
+
+namespace EsRadAppGenerator\EntityStuff\Output;
+
+use PHPUnit\Framework\TestCase;
+
+class EntityTest extends TestCase
+{
+    function test_it_can_merge_entities_and_dedupe_properties()
+    {
+        $entityA = Entity::new(
+            'Test',
+            PropertyCollection::with([
+                Property::new('foo', 'string'),
+                Property::new('bar', 'int'),
+            ])
+        );
+        $entityB = Entity::new(
+            'Test',
+            PropertyCollection::with([
+                Property::new('baz', 'string'),
+            ])
+        );
+
+        $entityAExpected = Entity::new(
+            'Test',
+            PropertyCollection::with([
+                Property::new('foo', 'string'),
+                Property::new('bar', 'int'),
+                Property::new('baz', 'string'),
+            ])
+        );
+
+        $entityA->merge($entityB);
+        
+        $this->assertEquals($entityAExpected, $entityA);
+    }
+
+    function test_it_throws_exception_when_merging_entities_with_different_classes()
+    {
+        $entityA = Entity::new(
+            'Test',
+            PropertyCollection::with([
+            ])
+        );
+        $entityB = Entity::new(
+            'NOTTest',
+            PropertyCollection::with([
+            ])
+        );
+
+        $this->expectException(\InvalidArgumentException::class);
+
+        $entityA->merge($entityB);
+    }
+}
---
+++ tests/EsRadAppGenerator/EntityStuff/Output/EventTest.php
@@ -0,0 +1,57 @@
+<?php
+declare(strict_types=1);
+
+namespace EsRadAppGenerator\EntityStuff\Output;
+
+use PHPUnit\Framework\TestCase;
+
+class EventTest extends TestCase
+{
+    function test_it_can_merge_events_and_dedupe_properties()
+    {
+        $eventA = Event::new(
+            'Test',
+            PropertyCollection::with([
+                Property::new('foo', 'string'),
+                Property::new('bar', 'int'),
+            ])
+        );
+        $eventB = Event::new(
+            'Test',
+            PropertyCollection::with([
+                Property::new('baz', 'string'),
+            ])
+        );
+
+        $eventAExpected = Event::new(
+            'Test',
+            PropertyCollection::with([
+                Property::new('foo', 'string'),
+                Property::new('bar', 'int'),
+                Property::new('baz', 'string'),
+            ])
+        );
+
+        $eventA->merge($eventB);
+        
+        $this->assertEquals($eventAExpected, $eventA);
+    }
+
+    function test_it_throws_exception_when_merging_events_with_different_classes()
+    {
+        $entityA = Event::new(
+            'Test',
+            PropertyCollection::with([
+            ])
+        );
+        $entityB = Event::new(
+            'NOTTest',
+            PropertyCollection::with([
+            ])
+        );
+
+        $this->expectException(\InvalidArgumentException::class);
+
+        $entityA->merge($entityB);
+    }
+}
